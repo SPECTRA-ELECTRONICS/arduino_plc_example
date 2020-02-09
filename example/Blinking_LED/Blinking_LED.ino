919ff0111b78203b5b920c1dca9dfd8953e66994
Hamza BENDALI BRAHAM <hbendali@ya.ru>
2020-02-09 09:18:04 +0100

    update

--- example/Blinking_LED/Blinking_LED.ino
+++ example/Blinking_LED/Blinking_LED.ino
@@ -1,6 +1,6 @@
 #include <spc_ard_plc.h>
-
-SPC_ARD_PLC plc(ARD_PLC_A10);   // declare plc as SPC_ARD_PLC class and config to use A10 board 
+#define SPC_ARD_PLC_A10
+SPC_ARD_PLC plc;   // declare plc as SPC_ARD_PLC class and config to use A10 board 
 
 #define LED RL1                 // define LED macro as RL1
 
