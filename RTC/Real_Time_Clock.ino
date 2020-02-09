32634c2c672bb5dfd6f0392d772652412b48131c
Hamza BENDALI BRAHAM <hbendali@ya.ru>
2020-02-09 09:58:51 +0100

    RTC in progress ...

---
+++ RTC/Real_Time_Clock.ino
@@ -0,0 +1,11 @@
+#include <spc_ard_plc.h>
+#define SPC_ARD_PLC_A10
+SPC_ARD_PLC plc;   // declare plc as SPC_ARD_PLC class and config to use A10 board 
+
+void setup() {
+    // ...
+}
+
+void loop() {
+    // ...
+}
